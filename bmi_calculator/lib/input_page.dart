de16d23257e7ef8a840715835377331f6d1f6ca4
QaisSherazi <101888152+QaisSherazi@users.noreply.github.com>
2022-04-12 18:40:21 +0500

    UI of BMI

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -15,9 +15,54 @@ class _InputPagestate extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child:Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Container(
+          margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+            borderRadius: BorderRadius.circular(10.0)
       ),
-    );
+      ),),
+              Expanded(child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)
+    ),
+    ),),
+            ],
+          ),),
+          Expanded(child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)
+    ),
+    ),),
+          Expanded(child: Row(
+          children: [
+          Expanded(child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)
+    ),
+    ),),
+    Expanded(child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)
+    ),
+    ),),
+    ],
+    ),
+          ),]),
+
+      );
+
   }
 }
