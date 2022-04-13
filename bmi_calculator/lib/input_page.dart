688ff8f1e48914a8480d89e59bb00da09214755c
QaisSherazi <101888152+QaisSherazi@users.noreply.github.com>
2022-04-13 15:48:51 +0500

    Use of Reafctor Code (ICON,LABEL)

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
 
@@ -21,10 +22,18 @@ class _InputPagestate extends State<InputPage> {
             children: [
               Expanded(child:new RepeatContainerCode(
                 colors: Color((0xFF1D1E33)),
+                cardWidget: RepeatTextandIconWidget(
+                  iconData:FontAwesomeIcons.male,
+                  label: 'MALE',
+                ),
               ),
     ),
           Expanded(child: RepeatContainerCode(
             colors: Color((0xFF1D1E33)),
+            cardWidget: RepeatTextandIconWidget(
+              iconData:FontAwesomeIcons.female,
+              label: 'FEMALE',
+            ),
           ),
           ),
           ],
@@ -32,16 +41,28 @@ class _InputPagestate extends State<InputPage> {
           ),
           Expanded(child: RepeatContainerCode(
             colors: Color((0xFF1D1E33)),
+            cardWidget: RepeatTextandIconWidget(
+              iconData:FontAwesomeIcons.female,
+              label: 'FEMALE',
+            ),
           ),
           ),
           Expanded(child: Row(
           children: [
           Expanded(child:  RepeatContainerCode(
             colors: Color((0xFF1D1E33)),
+            cardWidget: RepeatTextandIconWidget(
+              iconData:FontAwesomeIcons.female,
+              label: 'FEMALE',
+            ),
           ),
     ),
     Expanded(child: RepeatContainerCode(
       colors: Color((0xFF1D1E33)),
+      cardWidget: RepeatTextandIconWidget(
+        iconData:FontAwesomeIcons.female,
+        label: 'FEMALE',
+      ),
     ),),
     ],
     ),
@@ -52,13 +73,40 @@ class _InputPagestate extends State<InputPage> {
   }
 }
 
+class RepeatTextandIconWidget extends StatelessWidget {
+  RepeatTextandIconWidget({required this.iconData,required this.label});
+final IconData iconData;
+final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          FontAwesomeIcons.male,
+          size: 85.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text('MALE', style: TextStyle(
+          fontSize: 30.0,
+        ),
+        )],
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({required this.colors});
+  RepeatContainerCode({required this.colors,required this.cardWidget});
   Color colors;
+  final Widget cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
           margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: colors,
             borderRadius: BorderRadius.circular(10.0)
