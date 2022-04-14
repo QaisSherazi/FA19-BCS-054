89fd4be890d9bca1a1c92bc739e735171cb1a922
QaisSherazi <101888152+QaisSherazi@users.noreply.github.com>
2022-04-14 17:53:27 +0500

    Floating actionButton

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,5 +1,4 @@
 import 'dart:html';
-
 import 'package:bmi_calculator/constantfile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +19,7 @@ class _InputPagestate extends State<InputPage> {
    Gender selectGender;
    int sliderHeight=180;
    int sliderWeight=60;
+   int sliderAge=20;
    @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,8 +44,7 @@ class _InputPagestate extends State<InputPage> {
                     label: 'MALE',
                   ),
                 ),
-
-    ),
+              ),
           Expanded(
             child: RepeatContainerCode(
               onPressed: (){
@@ -100,25 +99,93 @@ class _InputPagestate extends State<InputPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text('WEIGHT',style: klabel,),
-                Text(sliderWeight.toString(),style: knumber,)
+                Text(sliderWeight.toString(),style: knumber,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RoundIcon(
+                        iconData:FontAwesomeIcons.minus,
+                        onPress:(){
+                        setState(() {
+                          sliderWeight--;
+                        });
+                        }
+                    ),
+                    SizedBox(width: 10.0,),
+                    RoundIcon(
+                        iconData:FontAwesomeIcons.plus,
+                        onPress:(){
+                          setState(() {
+                            sliderWeight++;
+                          });
+                        }
+                    ),
+                  ],
+                )
               ],
             )
           ),
     ),
     Expanded(child: RepeatContainerCode(
       colors: Color((0xFF1D1E33)),
-      cardWidget: RepeatTextandIconWidget(
-        iconData:FontAwesomeIcons.female,
-        label: 'FEMALE',
+        cardWidget: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('AGE',style: klabel,),
+            Text(sliderAge.toString(),style: knumber,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RoundIcon(
+                    iconData:FontAwesomeIcons.minus,
+                    onPress:(){
+                      setState(() {
+                      sliderAge--;
+                      });
+                    }
+                ),
+                SizedBox(width: 10.0,),
+                RoundIcon(
+                    iconData:FontAwesomeIcons.plus,
+                    onPress:(){
+                      setState(() {
+                        sliderAge++;
+                      });
+                    }
+                ),
+              ],
+            )
+          ],
+        )
       ),
-    ),),
+    ),
     ],
     ),
           ),]),
-
-      );
-
-  }
+    );
+   }
 }
 
 
+class RoundIcon extends StatelessWidget {
+  RoundIcon({@required this.iconData, @required this.onPress});
+
+  final IconData iconData;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(iconData),
+      onPressed: onPress,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        height: 56.0,
+        width: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+
+    );
+  }
+}
